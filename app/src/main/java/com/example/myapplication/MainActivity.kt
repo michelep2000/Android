c9dd228e0024df3dfd492f421225867bd5bdc2d4
Michele <miguelalfonsopardo@gmail.com>
2019-10-13 13:58:54 +0200

    Exercise 2 finished
    -Creado boton para ir a la activity perfil
    -Listener con intent pasandole por parametro el contenido
    -Creados TextViews necesarios para la captura de los
    parametros(descripcion, ciudad, imagen, fechaNacimiento)
    -Seteados los parametros (imagen y descripcion desempaquetados por no
    aceptar un valor opcional)
    -Añadidos nuevos strings, a los xml de idiomas, en español e ingles
    -Añadido ScrollView para evitar que la descripcion se corte

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,14 +12,20 @@ import kotlin.time.milliseconds
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var txtHello: TextView
+    private lateinit var btnClickMe: Button
+    private lateinit var counter: TextView
+    private lateinit var btnProfile: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //sacando el id directamente del xml de activity_main
-        val txtHello: TextView = findViewById(R.id.txtHello)
-        val btnClickMe: Button = findViewById(R.id.btnClickMe)
-        val counter: TextView = findViewById(R.id.counter)
+        txtHello = findViewById(R.id.txtHello)
+        btnClickMe = findViewById(R.id.btnClickMe)
+        counter = findViewById(R.id.counter)
+        btnProfile = findViewById(R.id.profile_button)
 
         //sacando el dato directamente del xml de idiomas
         val message = getString(R.string.youClickMe)
@@ -26,13 +33,22 @@ class MainActivity : AppCompatActivity() {
 
         btnClickMe.setOnClickListener {
 
-            count++
             txtHello.text = "U-Tad"
-            Toast.makeText(this, message+ count.toString()+ " times",Toast.LENGTH_SHORT).show()
-            counter.text = count.toString()
+            incrementText(count++, counter)
         }
 
+        btnProfile.setOnClickListener{
+            val intent = Intent(this, ProfileActivity::class.java)
+            intent.putExtra("description", R.string.description)
+            intent.putExtra("city", "Madrid")
+            intent.putExtra("birthdate", "28/09/2000")
+            intent.putExtra("image", R.drawable.boy)
+            startActivity(intent)
+        }
+    }
 
+    fun incrementText(count: Int, counter: TextView) {
+        counter.text = count.toString()
     }
 }
 
