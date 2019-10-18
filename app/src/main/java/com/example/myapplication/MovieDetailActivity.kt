4337a243568fabd4c98039e10b7b93a5323b72c3
Michele <miguelalfonsopardo@gmail.com>
2019-10-18 13:47:14 +0200

    Classroom practice movieActivity

--- app/src/main/java/com/example/myapplication/MovieDetailActivity.kt
+++ app/src/main/java/com/example/myapplication/MovieDetailActivity.kt
@@ -2,7 +2,6 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.example.myapplication.model.Movie
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_movie_detail.*
@@ -13,8 +12,6 @@ class MovieDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
-        print("paso contentView")
-
 
         movie = Movie(
             img = "https://i.imgur.com/T0YDgh5.png",
@@ -24,16 +21,11 @@ class MovieDetailActivity : AppCompatActivity() {
             description = "Los cazadores de zombis viajan desde la Casa Blanca hasta el corazón de los Estados Unidos, donde tendrán que defenderse de nuevas clases de muertos vivientes que han evolucionado. Mientras intentan salvar el mundo, los miembros de la pandilla también tendrán que aprender a convivir"
         )
 
-        print("paso movie")
         with(movie) {
             Picasso.get().load(img).into(movieImg)
-            print("paso picasso")
             movieTitle.text = title
-            print("paso title")
             txtYearValue.text = year
-            print("paso year")
             txtGenreValue.text = genre
-            print("paso genre")
             txtDescriptionMovie.text = description
         }
 
